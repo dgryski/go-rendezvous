b2d1dac4b23eeed87deae7160de90f7d4d7f04ad
Ryan Slade <ryanslade@gmail.com>
2021-09-09 14:11:53 +0200

    Benchmark over a range of node counts

--- rdv_test.go
+++ rdv_test.go
@@ -21,13 +21,13 @@ func TestEmpty(t *testing.T) {
 }
 
 func TestLookupN(t *testing.T) {
-	r := New(nodes(), xxhash.Sum64String)
+	r := New(nodes(defaultNodeCount), xxhash.Sum64String)
 	t.Log(r.Lookup("hello"))
 	t.Log(r.LookupN("hello", 3))
 }
 
 func TestLookupN_Lookup_Equivalence(t *testing.T) {
-	r := New(nodes(), hashString)
+	r := New(nodes(defaultNodeCount), hashString)
 
 	prop := func(k string) bool {
 		a := r.LookupN(k, 1)[0]
@@ -42,22 +42,34 @@ func TestLookupN_Lookup_Equivalence(t *testing.T) {
 
 func BenchmarkLookup(b *testing.B) {
 	b.ReportAllocs()
-	r := New(nodes(), xxhash.Sum64String)
-	for i := 0; i < b.N; i++ {
-		r.Lookup("github.com/foo/bar")
+	for _, nodeCount := range []int{10, 100, 1000} {
+		b.Run(fmt.Sprintf("NodeCount-%d", nodeCount), func(b *testing.B) {
+			r := New(nodes(nodeCount), xxhash.Sum64String)
+			for i := 0; i < b.N; i++ {
+				r.Lookup("github.com/foo/bar")
+			}
+		})
 	}
 }
 
 func BenchmarkLookupN(b *testing.B) {
 	b.ReportAllocs()
-	r := New(nodes(), xxhash.Sum64String)
-	for i := 0; i < b.N; i++ {
-		r.LookupN("github.com/foo/bar", 3)
+	b.ReportAllocs()
+	for _, nodeCount := range []int{10, 100, 1000} {
+		b.Run(fmt.Sprintf("NodeCount-%d", nodeCount), func(b *testing.B) {
+			r := New(nodes(nodeCount), xxhash.Sum64String)
+			for i := 0; i < b.N; i++ {
+				r.LookupN("github.com/foo/bar", 3)
+			}
+
+		})
 	}
 }
 
-func nodes() []string {
-	nodes := make([]string, 24)
+const defaultNodeCount = 24
+
+func nodes(n int) []string {
+	nodes := make([]string, n)
 	for i := 0; i < cap(nodes); i++ {
 		nodes[i] = fmt.Sprintf("indexed-search-%d.indexed-search:6070", i)
 	}
